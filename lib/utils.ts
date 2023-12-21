3331d408d48d570106c3759b9ed106b22360fb08
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-12-21 19:53:29 +0100

    previous commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+// Is enabling to safely and properly combine tailwind classes, more dinamically
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
